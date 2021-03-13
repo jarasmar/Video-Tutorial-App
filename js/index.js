2bb28d93053a6d01ef2ae483fce3fb1a3504b655
Jara <jarasmar@gmail.com>
2021-03-13 12:46:41 +0000

    Make API call and build basic search result structure

--- js/index.js
+++ js/index.js
@@ -4,10 +4,23 @@ function downloadAllVideos() {
     url: "https://lingumi-take-home-test-server.herokuapp.com/videoTutorials/",
     type: "GET",
     success: function(result) {
-      console.log(result);
+      buildResultList(result)
     },
     error: function(error) {
       console.log(error);
     }
   })
+}
+
+// Build List with API result
+function buildResultList(list) {
+  var listContainer = $("#result-list")
+  
+  list.forEach(function(video) {
+    $(listContainer).append(`<li class="list-item" id="${video.id}">\
+    <span><a href="${video.videoUrl}" target=blank>${video.videoTitle}</a></span>\
+    <span>Tags: ${video.tags}</span>\
+    <span>Teacher: ${video.teacherName}</span>\
+    </li>`)
+  });
 }
